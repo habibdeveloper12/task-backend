b6eea9b758681e892d3b1b4acc029353a3713eab
habibur rahman <habibur.rahman@elementzerolabs.com>
2023-11-21 22:07:42 +0600

    f

--- dist/app/utils/generateUniqueId.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const uuid_1 = require("uuid");
-function generateUniqueId() {
-    return (0, uuid_1.v4)();
-}
-exports.default = generateUniqueId;
